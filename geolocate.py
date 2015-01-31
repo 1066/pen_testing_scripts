6bb6e78d5e6dc78713627dda096df0e5d7c11cf9
1066 <github@1066.io>
2015-01-31 10:03:54 +0000

    added a few minor changes

--- geolocate.py
+++ geolocate.py
@@ -6,4 +6,12 @@ def printRecord(gt):
 	rec = gi.record_by_name(gt)
 	city = rec['city']
 	region = rec['region_name']
-	country = rec
+	country = rec['country_name']
+	long = rec['longitude']
+	lat = rec['latitude']
+	print '[*] Target: ' + ' Geo-located. '
+	print '[+] '+str(city)+', '+star(region)+' '+str(country)
+	print '[+] Latitude: '+str(lat)+ ', Longitude: '+ str(long)
+tgt = '173.255.226.98'
+printRecord(tgt)
+
